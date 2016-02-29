7c08cd02afe6c6bf76ab7fc1b7bb60913e6a72ca
lemon-zf <zhang_91026@163.com>
2016-02-29 23:02:55 +0800

    ff

--- src/main/java/com/train/collection/comparison/guava/MultisetTest.java
+++ src/main/java/com/train/collection/comparison/guava/MultisetTest.java
@@ -14,32 +14,9 @@ public class MultisetTest {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-//		HashMultiset<String> multiSet = HashMultiset.create();
-//		
-//		List<String> s = Arrays.asList(temp);
-//		
-//		multiSet.addAll(s);
-//		
-//		System.out.println(multiSet.count("1"));
+		HashMultiset<String> multiSet = HashMultiset.create();
 		
-		List<String> list = new ArrayList<String>();
-		
-		for(int i=0;i<10000000;i++){
-			list.add(i+"");
-		}
-		
-//		Map<String,Integer> map = new HashMap<String, Integer>();
-////		
-//		for(int i=0;i<1000;i++){
-//			map.put(i+"",i);
-//		}
-//
-//		map.get(0+"");
-//		
-//		System.out.println(51 & 15);
-//		System.out.println(51 & 31);
-//		System.out.println(11118 & 15);
-//		System.out.println(11118 & 15);
+
 	}
 	
 }
